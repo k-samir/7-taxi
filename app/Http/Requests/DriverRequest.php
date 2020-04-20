ab1f9a524da33e1600d36b5986abafe4d692f557
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-04-20 17:42:55 -0400

    Ajout de controller, request et model pour le formulaire

--- app/Http/Requests/DriverRequest.php
+++ app/Http/Requests/DriverRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
-class FormChauffeurRequest extends FormRequest
+class ChauffeurRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
