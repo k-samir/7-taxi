a26fad5fef1d39363dfb117042ee6a2a198b14ac
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-04-27 17:36:03 -0400

    Déplacement plus intuitif de l'accueil. C'est maintenant au titre lui-même.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -25,14 +25,13 @@
 <body id="base-body">
 <div id="navigation-bar" class="container-fluid mb-auto navbar-light bg-light">
     <nav class="navbar navbar-expand-lg">
-        <div class="navbar-brand"><h2 class="m-0 font-weight-bolder">Projet - Taxi</h2></div>
+        <a class="navbar-brand" href="{{url('/')}}"><h2 class="m-0 font-weight-bolder">Projet - Taxi</h2></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item font-weight-bold"><a class="nav-link" href="{{url('/')}}">Accueil</a></li>
                 <li class="nav-item dropdown font-weight-bold">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formulaires de chauffeur</a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
