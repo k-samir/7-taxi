3b43f988868a131af23d6618f37b995c43537156
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-04-27 17:23:04 -0400

    supression de la bordure du bas dans le navbar

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -23,7 +23,7 @@
 </head>
 
 <body id="base-body">
-<div class="container-fluid mb-auto border-bottom">
+<div class="container-fluid mb-auto">
     <nav id="navigation-bar" class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="navbar-brand"><h2 class="m-0 font-weight-bolder">Projet - Taxi</h2></div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
