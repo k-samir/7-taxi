08da149e90848883f4ab1169994caab23c50d596
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-04-27 16:28:18 -0400

    optimisation du code dans la méthode updateRecetteReel()

--- resources/views/FormulaireChauffeur.blade.php
+++ resources/views/FormulaireChauffeur.blade.php
@@ -2,14 +2,13 @@
 @section('title','Formulaire pour les chauffeurs')
 
 @section('before-scripts')
-    <script> 
-        function updateRecetteReel(){
-           var recetteInit = document.getElementById('recetteInit').value != '' ? parseInt(document.getElementById('recetteInit').value) :0 ;
-           var recetteFinal = document.getElementById('recetteFinal').value != '' ?parseInt(document.getElementById('recetteFinal').value) :0 ;
-           var prixFixe = document.getElementById('prixFixe').value != '' ? parseInt(document.getElementById('prixFixe').value) :0;
-           console.table(recetteInit,recetteFinal,prixFixe);
-           var reel = (recetteFinal - recetteInit) + prixFixe;
-           document.getElementById('recetteReel').value = reel;
+    <script>
+        function updateRecetteReel() {
+            let startingRecipe = parseInt(document.getElementById('recetteInit').value);
+            let endingRecipe = parseInt(document.getElementById('recetteFinal').value);
+            let fixPrice = parseInt(document.getElementById('prixFixe').value);
+
+            document.getElementById('recetteReel').value = (endingRecipe - startingRecipe) + fixPrice;
         }
     </script>
 @endsection
@@ -22,7 +21,7 @@
                 <div class="col">
                     <div class="group-control">
                         <label for="numTaxi">Numéro du taxi</label>
-                        <input type="text" name="numTaxi" id="numTaxi" class="form-control" >
+                        <input type="text" name="numTaxi" id="numTaxi" class="form-control">
                     </div>
                 </div>
                 <div class="col">
@@ -34,7 +33,7 @@
                 <div class="col">
                     <div class="group-control">
                         <label for="numTaxi">Date</label>
-                        <input type="datetime-local" name="numTaxi" class="form-control" >
+                        <input type="datetime-local" name="numTaxi" class="form-control">
                     </div>
                 </div>
             </div>
@@ -42,19 +41,19 @@
                 <div class="col">
                     <div class="group-control">
                         <label for="numTaxi">Recette initial</label>
-                        <input type="number" name="recetteInit" id="recetteInit"class="form-control" onchange="updateRecetteReel()">
+                        <input type="number" name="recetteInit" id="recetteInit" class="form-control"onchange="updateRecetteReel()">
                     </div>
                 </div>
                 <div class="col">
                     <div class="group-control">
                         <label for="numTaxi">Recette final</label>
-                        <input type="number" name="recetteFinal" id="recetteFinal" class="form-control" onchange="updateRecetteReel()">
+                        <input type="number" name="recetteFinal" id="recetteFinal" class="form-control"onchange="updateRecetteReel()">
                     </div>
                 </div>
                 <div class="col">
                     <div class="group-control">
                         <label for="diff">Prix fixe</label>
-                        <input type="number" name="prixFixe" id="prixFixe" class="form-control" onchange="updateRecetteReel()">
+                        <input type="number" name="prixFixe" id="prixFixe" class="form-control"onchange="updateRecetteReel()">
                     </div>
                 </div>
                 <div class="col">
