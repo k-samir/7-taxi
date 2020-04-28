f925397b127ee3f7d9a9615d9f44a7406b8ff62b
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-04-28 15:59:48 -0400

    Déplacement des dépendances de "layout.app" à "layout.base"

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,13 +3,14 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     @yield('headContent')
-
 
     @yield('before-scripts')
 
@@ -18,6 +19,7 @@
             background-color: #cbca89;
         }
     </style>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset('css/font-color.css')}}">
     @yield('styles')
 </head>
