ecad5a913068a9313a419f31c8497a2b32368f81
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-05-04 13:10:24 -0400

    Simplification de la page d'accueil.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+@section('body-content')
+    <div class="container">
+        <div class="text-center"><h2><u>Vous êtes connecté</u></h2></div>
     </div>
-</div>
 @endsection
