2ac7b474f134a5733ea585103a9179cf27e4ab21
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-05-11 14:52:20 -0400

    application de mes modifications suite au merge

--- resources/ts/formDriver.ts
+++ resources/ts/formDriver.ts
@@ -4,13 +4,18 @@ export namespace src{export namespace form {
         return (<HTMLInputElement>document.getElementById(id));
     }
 
-
     function convertToFloat(htmlElement: HTMLInputElement): number
     function convertToFloat(value: string): number
     function convertToFloat(var1: string | HTMLInputElement): number {
         return typeof var1 === "string" ?
             var1 == "" ? 0 : parseFloat(var1) :
             convertToFloat(var1.value);
+    }
+
+    function setSum(elementToSet: HTMLInputElement, elementToSum: number[]): void {
+        let sum = 0;
+        elementToSum.forEach(element => sum += element);
+        elementToSet.value = String(sum);
     }
 
     export class FormDriver {
@@ -26,16 +31,13 @@ export namespace src{export namespace form {
             getID("salary").value = String(convertToFloat(getID("realRecipe")) * this.commission);
         }
 
-        public setDifference(start: string,end:string, diff:string): void{
-            const depart = getID(start).value != "" ? parseFloat(getID(start).value) :0;
-            const arrive = getID(end).value != "" ? parseFloat(getID(end).value) :0;
-            getID(diff).value = (arrive - depart).toString();
+        public setDifference(elementToSetID: string, startingID: string, endingID: string): void {
+            getID(elementToSetID).value = String(convertToFloat(getID(startingID)) - convertToFloat(getID(endingID)));
         }
-
 
         /**
          * Method to calculate the real recipe from the difference of initial recipe and final recipe plu the fix price.<br>
-         * If the realRecipe is greater than 0, then, the method {@link updateSalary} will be called.
+         * Afterward, the method {@link updateSalary} will be called.
          */
         public updateRealRecipe(): void {
             let startingRecipe = convertToFloat(getID('startRecipe'));
@@ -44,9 +46,49 @@ export namespace src{export namespace form {
 
             let realRecipe = endingRecipe - startingRecipe + fixPrice;
             getID('realRecipe').value = String(realRecipe);
-            if (realRecipe > 0) this.updateSalary();
+            this.updateSalary();
+            this.__updateTotalExpenses();
+            this.updateTotalNet();
         }
 
+        public updateTotalNet(): void {
+            this.setDifference("totalNet", "totalExpenses", "realRecipe");
+        }
+
+        private __updateTotalExpenses(): void {
+            this.setSum("totalExpenses", "salary", "gaz", "credit", "various");
+        }
+
+        public updateTotalExpenses(): void {
+            this.__updateTotalExpenses();
+            this.updateTotalNet();
+        }
+
+        public setSum(elementIDToSet: string, ...elementIdsToRetrieveTheSum: string[]): void {
+            let tempElementToRetrieveTheSum: number[] = [];
+            for (let i = 0; i < elementIdsToRetrieveTheSum.length; i++)
+                tempElementToRetrieveTheSum[i] = convertToFloat(getID(<string>elementIdsToRetrieveTheSum[i]))[i];
+
+            setSum(typeof elementIDToSet === "string" ? getID(elementIDToSet) : elementIDToSet, tempElementToRetrieveTheSum);
+        }
+
+        public onNumberModification(callback: () => void, ...ids: string[]) {
+            ids.forEach(id => {
+                let element = getID(id);
+                element.onchange = () => {
+                    //Serve when the user change the focus to another field or use the field modification (when it's a number/date)
+                    callback();
+                }
+                element.oninput = () => {
+                    //On any change from the input.
+                    callback();
+                }
+                element.onkeydown = (keyEvent) => {
+                    //On the key DELETE, BACKSPACE or just a number, the callback is executed if the others fail.
+                    if (keyEvent.key === "Backspace" || keyEvent.key === "Delete" || keyEvent.key.match(/\d/)) callback();
+                };
+            });
+        }
     }
 
 }}
@@ -54,17 +96,16 @@ export namespace src{export namespace form {
 import FormDriver = src.form.FormDriver;
 
 (() => {
-//Export global methods and variable
+    //Apply the triggers on the elements
 
-let form = new FormDriver();
+    let form = new FormDriver();
 
-window['updateSalary'] = () => form.updateSalary();
-window['updateRealRecipe'] = () => form.updateRealRecipe();
-
-window['setDifferenceOnMillage'] = () => form.setDifference("startingMillage", "endingMillage", "totalMillage");
-window['setDifferenceOnMillageLaden'] = () => form.setDifference("startingMileageLaden", "endingMileageLaden", "totalMileageLaden");
-window['setDifferenceOnAmountOfPassengers'] = () => form.setDifference("startingAmountOfPassengers", "endingAmountOrPassengers", "totalAmountOfPassengers");
-window['setDifferenceOnMileageInVehicle'] = () => form.setDifference("startingMileageInVehicle", "endingMileageInVehicle", "totalMileageInVehicle");
-
+    form.onNumberModification(() => form.updateRealRecipe(), "startRecipe", "finalRecipe", "fixPrice");
+    form.onNumberModification(() => form.updateSalary(), "realRecipe");
+    form.onNumberModification(() => form.setDifference("totalMillage", "startingMillage", "endingMillage"), "startingMillage", "endingMillage");
+    form.onNumberModification(() => form.setDifference("totalMileageLaden", "startingMileageLaden", "endingMileageLaden"), "startingMileageLaden", "endingMileageLaden");
+    form.onNumberModification(() => form.setDifference("totalAmountOfPassengers", "startingAmountOfPassengers", "endingAmountOrPassengers"), "startingAmountOfPassengers", "endingAmountOrPassengers");
+    form.onNumberModification(() => form.setDifference("totalMileageInVehicle", "startingMileageInVehicle", "endingMileageInVehicle"), "startingMileageInVehicle", "endingMileageInVehicle");
+    form.onNumberModification(() => form.updateTotalExpenses(), "salary", "gaz", "credit", "various");
 
 })();
