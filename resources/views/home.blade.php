75a802f7eda459e26b213499a7b411d307d4d854
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-05-11 16:45:36 -0400

    Ça ne sert pas à grand chose d'avoir un message pour la page d'accueil.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,1 @@
 @extends('layouts.base')
-
-@section('body-content')
-    <div class="container mb-5">
-        <div class="text-center"><h2><u>Vous êtes connecté</u></h2></div>
-    </div>
-@endsection
