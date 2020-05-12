153c62cc54473f96e44a9f82d957d409b03fec7f
Samir Kamar <k-samir@live.fr>
2020-05-12 16:19:05 -0400

    Màj layout avec design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,134 @@
 @extends('layouts.base')
+@section('title', "Page d'accueil")
+
+@section('body-content')
+
+
+<body id="page-top">
+
+    <header class="masthead" style="background-image: url(&quot;img/taxi-background.jpg&quot;);background-position: center;">
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in"><span>Bienvenue chez 7-Taxi</span></div>
+                <div class="intro-heading text-uppercase"><span>VoyageZ AVEC NOUS</span></div><a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" role="button" href="#services">COMMENCER</a></div>
+        </div>
+    </header>
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">Services</h2>
+                    <h3 class="text-muted section-subheading">Lorem ipsum dolor sit amet consectetur</h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-6"><span class="fa-stack fa-4x"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-plane fa-stack-1x fa-inverse"></i></span>
+                    <h4 class="section-heading">Transport</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                </div>
+                <div class="col-md-6"><span class="fa-stack fa-4x"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-smile-o fa-stack-1x fa-inverse"></i></span>
+                    <h4 class="section-heading">Assistance</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio" class="bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">L'ENTREPRISE</h2>
+                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-4 portfolio-item">
+                    <a class="portfolio-link">
+                        <div class="portfolio-hover"></div><img class="img-fluid" src="img/Depositphotos_110892424_l-2015-1600x1067.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>Qualité</h4>
+                        <p class="text-muted">Illustration</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item">
+                    <a class="portfolio-link">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"></div>
+                        </div><img class="img-fluid" src="img/img-texte-pv-no6.png"></a>
+                    <div class="portfolio-caption">
+                        <h4>Sûreté</h4>
+                        <p class="text-muted"></p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item">
+                    <a class="portfolio-link">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"></div>
+                        </div><img class="img-fluid" src="img/45003.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>Professionalisme</h4>
+                        <p class="text-muted">Identity</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase">A PROPOS DE NOUS</h2>
+                    <h3 class="text-muted section-subheading">Notre Histoire</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-group timeline">
+                        <li class="list-group-item">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="img/about/1.jpg"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>&nbsp;Mars 2020</h4>
+                                    <h4 class="subheading">Commencement</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item timeline-inverted">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="img/rsz_code-editoren-t.jpg"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>Mai 2020</h4>
+                                    <h4 class="subheading">Dévellopement</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="img/rsz_lancement-site-web_1212-24.jpg"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>Juin 2020</h4>
+                                    <h4 class="subheading">Phase de lancement</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">dfgdgddgdgd</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+    
+
+
+</body>
+
+@endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,75 +8,193 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','Projet - Taxi')</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     @yield('headContent')
 
     @yield('before-scripts')
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset('css/font-color.css')}}">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="{{ asset('fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/-Login-form-Page-BS4-.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/Responsive-Form-1.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/Responsive-Form.css')}}">
     @yield('styles')
 </head>
 
 <body id="base-body">
-<div id="navigation-bar" class="container-fluid mb-3 navbar-light bg-light">
-    <nav class="navbar navbar-expand-md">
-        <a class="navbar-brand" href="{{route('home')}}"><h2 class="m-0 font-weight-bolder">Projet - Taxi</h2></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <div id="navigation-bar" class="container-fluid mb-3 navbar-light bg-light">
+        <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
+            <div class="container"><a class="navbar-brand" href="{{route('home')}}">7-Taxi</a><button
+                    data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right"
+                    type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false"
+                    aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="nav navbar-nav ml-auto text-uppercase">
+                        @guest
+                        <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                                href="{{route('login')}}">CONNEXION</a></li>
+                        @if (Route::has('register'))
+                        <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                                href="{{route('register')}}">INSCRIPTION</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#services">SERVICES</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#portfolio">L'ENTREPRISE</a>
+                        </li>
+                        <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#about">A
+                                PROPOS</a></li>
+                        <li class="nav-item" role="presentation"></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger"
+                                href="#contact">Contact</a></li>
+                        @endif
+                        @endguest
+                        @guest
+                        @if (Route::has('register'))
+                        @endif
+                        @else
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formulaires de
+                                    chauffeur</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('addConductorShift')}}">Ajout de shift</a>
+                                    <a class="dropdown-item" href="{{route('createConductor')}}">Création de
+                                        chauffeur</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formulaires de
+                                    créations</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('createConductor')}}">Création de
+                                        chauffeur</a>
+                                    <a class="dropdown-item" href="{{route('createClient')}}">Création de client</a>
+                                    <a class="dropdown-item" href="{{route('createTaxi')}}"> Création de taxi</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown font-weight-bold">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">Options</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <button type="button" id="btn-obscur-light" class="navigation-text dropdown-item"
+                                        data-toggle="button" aria-pressed="false" autocomplete="off"
+                                        onclick="changeDarkOrLightMode()">Mode obscur</button>
+                                    <button type="button" class="navigation-text dropdown-item" data-toggle="button"
+                                        aria-pressed="false" autocomplete="off"
+                                        onclick="changeAutomaticColor()">Couleurs automatique</button>
+                                </div>
+                            </li>
+                        </ul>
+                        @endguest
+                        @guest
+                        @if (Route::has('register'))
+                        @endif
+                        @else
+                        <li class="nav-item"><a class="nav-link" href="{{route('logout')}}"
+                                onclick="event.preventDefault();document.getElementById('logout-form').submit();">Déconnexion</a>
+                        </li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf
+                        </form>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-        <div id="navbarSupportedContent" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown font-weight-bold">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formulaires de chauffeur</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{route('addConductorShift')}}">Ajout de shift</a>
-                        <a class="dropdown-item" href="{{route('createConductor')}}">Création de chauffeur</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown font-weight-bold">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formulaires de créations</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{route('createConductor')}}">Création de chauffeur</a>
-                        <a class="dropdown-item" href="{{route('createClient')}}">Création de client</a>
-                        <a class="dropdown-item" href="{{route('createTaxi')}}"> Création de taxi</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown font-weight-bold">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <button type="button" id="btn-obscur-light" class="navigation-text dropdown-item" data-toggle="button" aria-pressed="false" autocomplete="off" onclick="changeDarkOrLightMode()">Mode obscur</button>
-                        <button type="button" class="navigation-text dropdown-item" data-toggle="button" aria-pressed="false" autocomplete="off" onclick="changeAutomaticColor()">Couleurs automatique</button>
-                    </div>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                @guest
-                    <li class="nav-item font-weight-bold ml-auto"><a class="nav-link" href="{{route('login')}}">Connexion</a></li>
-                    @if (Route::has('register'))
-                        <li class="nav-item font-weight-bold ml-auto"><a class="nav-link" href="{{route('register')}}">Inscription</a></li>
-                    @endif
-                @else
-                    <li class="nav-item font-weight-bold ml-auto"><a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Déconnexion</a></li>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
-                @endguest
-            </ul>
-        </div>
-    </nav>
-</div>
-@section('body-content')
-    <div class="row pb-3">
-        <h1 class="mx-auto"><u>@yield('title')</u></h1>
     </div>
-@show
+    @section('body-content')
+    <div class="row pb-3">
+        <h1 class="mx-auto" style="margin-top:9rem;"><u> @yield('title')</u></h1>
+    </div>
+    @show
+    <section id="contact" style="background-image:url('img/map-image.png');">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">NOUS CONTACTER</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form id="contactForm" name="contactForm" novalidate="novalidate">
+                        <div class="form-row">
+                            <div class="col col-md-6">
+                                <div class="form-group"><input class="form-control" type="text" id="name"
+                                        placeholder="Votre Nom *" required=""><small
+                                        class="form-text text-danger flex-grow-1 help-block lead"></small></div>
+                                <div class="form-group"><input class="form-control" type="email" id="email"
+                                        placeholder="Votre Email *" required=""><small
+                                        class="form-text text-danger help-block lead"></small></div>
+                                <div class="form-group"><input class="form-control" type="tel"
+                                        placeholder="Votre Numéro *" required=""><small
+                                        class="form-text text-danger help-block lead"></small></div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group"><textarea class="form-control" id="message"
+                                        placeholder="Votre Message *" required=""></textarea><small
+                                        class="form-text text-danger help-block lead"></small></div>
+                            </div>
+                            <div class="col">
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div><button class="btn btn-primary btn-xl text-uppercase"
+                                    id="sendMessageButton" type="submit">ENVOYER</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<scripts>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="{{asset('js/base.js')}}" defer></script>
-    @yield('after-scripts')
-</scripts>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><span class="copyright">Copyright&nbsp;© 7-Taxi 2020</span><span class="copyright"><br> J.Bédard - Y.Stephanus - S.Kamar</span></div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li class="list-inline-item"><a href="#" style="color:black;">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a href="#" style="color:black;">Terms of Use</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <scripts>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="assets/js/agency.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+        </script>
+        <script src="{{asset('js/base.js')}}" defer></script>
+
+        @yield('after-scripts')
+    </scripts>
 </body>
+
 </html>
