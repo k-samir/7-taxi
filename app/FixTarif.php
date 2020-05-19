caa5bd67e09dbd55aceadb407568b816f48eb8db
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-05-19 16:26:58 -0400

    Création de base des tarif fix (il reste à ajouter en BD)

---
+++ app/FixTarif.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use App\Http\Requests\FixTarifRequest;
+use Illuminate\Database\Eloquent\Model;
+
+class FixTarif extends Model
+{
+
+
+    public function addFixTarif(FixTarifRequest $request): void
+    {
+
+    }
+
+    public function updateFixTarif(FixTarifRequest $request): void
+    {
+
+    }
+
+
+}
