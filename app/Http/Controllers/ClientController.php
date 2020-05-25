fd02878f3824cbcf26534caef42e4393443f8ef1
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-05-25 15:07:45 -0400

    Formatage Et ajout du numéro de client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Client;
 use Illuminate\Http\Request;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 
 class ClientController extends Controller
@@ -18,23 +17,22 @@ class ClientController extends Controller
     public function createClient(Request $request)
     {
 
-
-        $out = new ConsoleOutput();
-        $out->writeln($request);
-
         $client = new Client();
+
+        $client->no_client = $request->input('clientNumber');
+        $client->solde_client = $request->input('solde');
 
         $client->prenom = $request->input('firstName');
         $client->nom = $request->input('lastName');
 
         $client->telephone = $request->input('phoneNumber');
         $client->email = $request->input('email');
+
         $client->no_rue = $request->input('streetNumber');
         $client->rue = $request->input('streetName');
         $client->ville = $request->input('cityName');
         $client->code_postal = $request->input('postalCode');
 
-        $client->solde_client = $request->input('solde');
         $client->save();
         return redirect()->route("home");
     }
--- resources/views/formulaireClient.blade.php
+++ resources/views/formulaireClient.blade.php
@@ -10,32 +10,33 @@
             @csrf
 
             <div class="row mb-4 justify-content-center">
-                <div class="col-4 input-group">
+                <div class="col input-group">
                     <label class="input-group-text" for="solde">Solde</label>
                     <input id="solde" class="form-control" value="{{old('solde', $solde ?? "")}}" type="number" name="solde" placeholder="Solde" required>
                 </div>
+                <div class="col input-group">
+                    <label class="input-group-text" for="phoneNumber">Numéro de téléphone</label>
+                    <input id="phoneNumber" class="form-control" value="{{old('phoneNumber', $phoneNumber ?? "")}}" type="tel" name="phoneNumber" placeholder="(123) 456-7890">
+                </div>
+                <div class="w-100"></div>
+                <div class="col"><span class="text-danger text-sm-right">@error('solde'){{$message}}@enderror</span></div>
+                <div class="col"><span class="text-danger text-sm-right">@error('phoneNumber'){{$message}}@enderror</span></div>
+            </div>
+            <div class="row mb-4 justify-content-center">
+
                 <div class="col input-group">
                     <label class="input-group-text" for="firstName">Prénom & Nom</label>
                     <input id="firstName" class="form-control" value="{{old('firstName', $firstName ?? "")}}" type="text" name="firstName" placeholder="Prénom" required>
                     <label class="input-group-text" for="lastName" hidden></label>
                     <input id="lastName" class="form-control" value="{{old('lastName', $lastName ?? "")}}" type="text" name="lastName" placeholder="Nom" required>
                 </div>
-                <div class="w-100"></div>
-                <div class="col-4"><span class="text-danger text-sm-right">@error('solde'){{$message}}@enderror</span></div>
-                <div class="col"><span class="text-danger text-sm-right">@error('firstName'){{$message}}@enderror</span></div>
-                <div class="col"><span class="text-danger text-sm-right">@error('lastName'){{$message}}@enderror</span></div>
-            </div>
-            <div class="row mb-4 justify-content-center">
-                <div class="col input-group">
-                    <label class="input-group-text" for="phoneNumber">Numéro de téléphone</label>
-                    <input id="phoneNumber" class="form-control" value="{{old('phoneNumber', $phoneNumber ?? "")}}" type="tel" name="phoneNumber" placeholder="(123) 456-7890">
-                </div>
                 <div class="col input-group">
                     <label class="input-group-text" for="email">Courriel</label>
                     <input id="email" class="form-control" value="{{old('email', $email ?? "")}}" type="tel" name="email" placeholder="email@template.com">
                 </div>
                 <div class="w-100"></div>
-                <div class="col"><span class="text-danger text-sm-right">@error('phoneNumber'){{$message}}@enderror</span></div>
+                <div class="col"><span class="text-danger text-sm-right">@error('firstName'){{$message}}@enderror</span></div>
+                <div class="col"><span class="text-danger text-sm-right">@error('lastName'){{$message}}@enderror</span></div>
                 <div class="col"><span class="text-danger text-sm-right">@error('email'){{$message}}@enderror</span></div>
             </div>
             <div class="row mb-4 justify-content-center">
@@ -53,13 +54,21 @@
                     <label class="input-group-text" for="postalCode">Code Postal</label>
                     <input id="postalCode" class="form-control" value="{{old('postalCode', $postalCode ?? "")}}" type="text" name="postalCode" placeholder="A1A 1A1">
                 </div>
-                <div class="w-100 pb-4"></div>
                 <div class="w-100"></div>
                 <div class="col"><span class="text-danger text-sm-right">@error('streetNumber'){{$message}}@enderror</span></div>
                 <div class="col"><span class="text-danger text-sm-right">@error('streetName'){{$message}}@enderror</span></div>
                 <div class="col-5"><span class="text-danger text-sm-right">@error('cityName'){{$message}}@enderror</span></div>
                 <div class="col"><span class="text-danger text-sm-right">@error('postalCode'){{$message}}@enderror</span></div>
             </div>
+
+        <div class="row mb-4 justify-content-center">
+            <div class="col input-group">
+                <label class="input-group-text" for="clientNumber">Numéro</label>
+                <input id="clientNumber" class="form-control" value="{{old('clientNumber', $clientNumber ?? "")}}" type="number" name="clientNumber" placeholder="Numéro">
+            </div>
+            <div class="w-100"></div>
+            <div class="col"><span class="text-danger text-sm-right">@error('number'){{$message}}@enderror</span></div>
+        </div>
 
             <div class="row">
                 <div class="col-4"></div>
