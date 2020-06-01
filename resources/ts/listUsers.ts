9c08fab06a02592d85d4a82610972b3fc41dae6b
Jonathan Bédard <1436298@cegeplimoilou.ca>
2020-06-01 17:27:04 -0400

    simple formatage de la classe lisUsers.ts

--- resources/ts/listUsers.ts
+++ resources/ts/listUsers.ts
@@ -1,24 +1,25 @@
 import {ajax} from "./AjaxCaller";
-import FormAjaxCaller = ajax.FormAjaxCaller;
+
 
 export namespace src.form {
+    import FormAjaxCaller = ajax.FormAjaxCaller;
 
     export class ListUsers {
 
         public static changeRole(id: number): void {
-            new FormAjaxCaller(<HTMLFormElement>document.getElementById("changeForm-" + id)).newCall();
+            new FormAjaxCaller(<HTMLFormElement>document.getElementById("changeForm-" + id))
+                .newCall();
         }
 
     }
 
+    (() => {
+        let $ = window['$'];
+
+        window['changeRole'] = (id: number) => ListUsers.changeRole(id);
+
+        $(document).ready(() => $('#listUsers').DataTable());
+    })();
 }
 
-import ListUsers = src.form.ListUsers;
 
-(() => {
-    let $ = window['$'];
-
-    window['changeRole'] = (id: number) => ListUsers.changeRole(id);
-
-    $(document).ready(() => $('#listUsers').DataTable());
-})();
